3960a988336b8d86e1a9927e6c9a17595892fc62
saad Kammous <saad.kammous1998@gmail.com>
2023-01-23 17:02:03 +0100

    correction project

--- src/test/java/com/kammous/demospring_cloudstreams_kafka/DemoSpringCloudStreamsKafkaApplicationTests.java
+++ src/test/java/com/kammous/demospring_cloudstreams_kafka/DemoSpringCloudStreamsKafkaApplicationTests.java
@@ -1,4 +1,4 @@
-package com.boukriinfo.demospring_cloudstreams_kafka;
+package com.kammous.demospring_cloudstreams_kafka;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
